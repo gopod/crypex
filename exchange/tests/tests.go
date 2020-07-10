383a31f2580cd5e03bdda7a3765c87caf25393b9
Sadegh Ramezani <sadegh@ramezaniu.me>
2020-07-10 19:49:31 +0430

    Move tests utils package

---
+++ exchange/tests/tests.go
@@ -0,0 +1,28 @@
+package tests
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// DefaultTimeout default tests timeout
+var DefaultTimeout = 3 * time.Second
+
+// ReceiveWithTimeout receive data from channel with timeout.
+func ReceiveWithTimeout(t *testing.T, ch <-chan interface{}, timeout *time.Duration) {
+	if timeout == nil {
+		timeout = &DefaultTimeout
+	}
+
+	for stay, timeout := true, time.After(*timeout); stay; {
+		select {
+		case <-timeout:
+			stay = false
+		case data, ok := <-ch:
+			t.Log(data)
+			assert.True(t, ok)
+		}
+	}
+}
