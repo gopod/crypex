3fc548450d40e5b379b9ee4d89e6934afcf49075
Sadegh Ramezani <sadegh@ramezaniu.me>
2020-07-10 21:45:26 +0430

    Update examples

--- example/general/general.go
+++ example/general/general.go
@@ -1,3 +1,37 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"github.com/ramezanius/crypex/exchange/hitbtc"
+	"github.com/ramezanius/crypex/exchange/hitbtc/converter"
+)
+
+func main() {
+	ToUSD()
+}
+
+const price, quantity = 10000.0, 10.0
+
+type repository struct{}
+
+// GetPrice returns fake price (BTC/USD)
+func (r *repository) GetPrice(_, _ string) float64 {
+	return price
+}
+
+// GetSymbol returns fake symbol detail (BTC/USD)[Demo]
+func (r *repository) GetSymbol(_, _ string) interface{} {
+	return &hitbtc.Symbol{
+		Base:  hitbtc.BTC,
+		Quote: hitbtc.USD,
+		ID:    hitbtc.Demo,
+	}
+}
+
+func ToUSD() {
+	cache := &repository{}
+	value := converter.ToUSD(cache, hitbtc.BTC, quantity, false)
+
+	log.Println(value)
+}
--- example/stream/stream.go
+++ example/stream/stream.go
@@ -1,3 +1,155 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"sync"
+
+	"github.com/ramezanius/crypex/exchange/binance"
+	"github.com/ramezanius/crypex/exchange/hitbtc"
+)
+
+var HitBTC *hitbtc.HitBTC
+var Binance *binance.Binance
+
+func setupExchange() {
+	var err error
+
+	HitBTC, err = hitbtc.New("YOUR_HITBTC_PUBLIC_KEY", "YOUR_HITBTC_SECRET_KEY")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	Binance, err = binance.New("YOUR_BINANCE_PUBLIC_KEY", "YOUR_BINANCE_SECRET_KEY")
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func main() {
+	setupExchange()
+
+	GetSymbol()
+	GetSymbols()
+
+	GetBalances()
+
+	NewOrder()
+	CancelOrder()
+	ReplaceOrder()
+
+	SubscribeReports()
+	SubscribeCandles()
+	UnsubscribeCandles()
+}
+
+func GetSymbol() {
+	symbol, err := HitBTC.GetSymbol(hitbtc.Demo)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Println(symbol)
+}
+
+func GetSymbols() {
+	symbols, err := HitBTC.GetSymbols()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Println(symbols)
+}
+
+func GetBalances() {
+	balances, err := HitBTC.GetBalances()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Println(balances)
+}
+
+func NewOrder() {
+	order, err := HitBTC.NewOrder(hitbtc.NewOrder{
+		Price:    2000,
+		Quantity: 0.00002,
+
+		Side:   hitbtc.Buy,
+		Type:   hitbtc.Limit,
+		Symbol: hitbtc.Demo,
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Println(order)
+}
+
+func CancelOrder() {
+	order, err := HitBTC.CancelOrder("FAKE_ORDER_ID")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Println(order)
+}
+
+func ReplaceOrder() {
+	order, err := HitBTC.ReplaceOrder(hitbtc.ReplaceOrder{
+		Price:    1000,
+		Quantity: 0.00001,
+		OrderID:  "FAKE_ORDER_ID",
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Println(order)
+}
+
+func SubscribeReports() {
+	reports, err := HitBTC.SubscribeReports()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	wg := sync.WaitGroup{}
+
+	wg.Add(1)
+	defer wg.Done()
+
+	for {
+		log.Println(<-reports)
+	}
+}
+
+func SubscribeCandles() {
+	candles, err := HitBTC.SubscribeCandles(
+		hitbtc.CandlesParams{
+			Limit:  100,
+			Symbol: hitbtc.Demo,
+			Period: hitbtc.Period1Minute,
+		})
+	if err != nil {
+		log.Panic(err)
+	}
+
+	wg := sync.WaitGroup{}
+
+	wg.Add(2)
+	defer wg.Done()
+
+	for {
+		log.Println(<-candles)
+	}
+}
+
+func UnsubscribeCandles() {
+	err := HitBTC.UnsubscribeCandles(
+		hitbtc.CandlesParams{
+			Symbol: hitbtc.Demo,
+		})
+	if err != nil {
+		log.Panic(err)
+	}
+}
