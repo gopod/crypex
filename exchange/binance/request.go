a0c881487c78bee3175a650248453b267ffb150d
Sadegh Ramezani <sadegh@ramezaniu.me>
2020-07-11 20:56:04 +0430

    Add requests package

--- exchange/binance/request.go
+++ exchange/binance/request.go
@@ -1,1 +1,112 @@
 package binance
+
+import (
+	"strings"
+
+	"github.com/ramezanius/crypex/exchange"
+)
+
+// SymbolsResponse struct
+type SymbolsResponse struct {
+	Data Symbols `json:"symbols,required"`
+}
+
+// GetSymbols gets a list of exchange symbols.
+func (b *Binance) GetSymbols() (response *Symbols, err error) {
+	var rawResponse SymbolsResponse
+
+	err = b.Request(exchange.RequestParams{
+		Method: "GET", Endpoint: "/exchangeInfo",
+	}, &rawResponse)
+	if err != nil {
+		return
+	}
+
+	response = &rawResponse.Data
+
+	return
+}
+
+// BalancesResponse struct
+type BalancesResponse struct {
+	Data Assets `json:"balances,required"`
+}
+
+// GetBalances gets user balances on exchange. @authenticate
+func (b *Binance) GetBalances() (response *Assets, err error) {
+	var rawResponse BalancesResponse
+
+	err = b.Request(exchange.RequestParams{
+		Method: "GET", Endpoint: "/account", Auth: true,
+	}, &rawResponse)
+	if err != nil {
+		return
+	}
+
+	response = &rawResponse.Data
+
+	return
+}
+
+// OrderResponse struct
+type OrderResponse Order
+
+// NewOrder struct
+type NewOrder struct {
+	Side  Side    `json:"side,required"`
+	Type  Type    `json:"type,required"`
+	Price float64 `json:"price,string"`
+
+	Symbol   string  `json:"symbol,required"`
+	Quantity float64 `json:"quantity,string"`
+	OrderID  string  `json:"newClientOrderId,omitempty"`
+
+	StopPrice   float64     `json:"stopPrice,omitempty"`
+	TimeInForce TimeInForce `json:"timeInForce,omitempty"`
+}
+
+// NewOrder places a new order. @authenticate
+func (b *Binance) NewOrder(params NewOrder) (response *Report, err error) {
+	params.Symbol = strings.ToUpper(params.Symbol)
+
+	var rawResponse OrderResponse
+
+	err = b.Request(exchange.RequestParams{
+		Auth: true, Params: params,
+		Method: "POST", Endpoint: "/order",
+	}, &rawResponse)
+	if err != nil {
+		return
+	}
+
+	report := Report(rawResponse)
+	response = &report
+
+	return
+}
+
+// CancelOrder cancels an order. @authenticate
+func (b *Binance) CancelOrder(orderID, symbol string) (response *Report, err error) {
+	params := struct {
+		Symbol  string `json:"symbol,required"`
+		OrderID string `json:"origClientOrderId,required"`
+	}{
+		OrderID: orderID,
+		Symbol:  strings.ToUpper(symbol),
+	}
+
+	var rawResponse OrderResponse
+
+	err = b.Request(exchange.RequestParams{
+		Auth: true, Params: params,
+		Method: "DELETE", Endpoint: "/order",
+	}, &rawResponse)
+	if err != nil {
+		return
+	}
+
+	report := Report(rawResponse)
+	response = &report
+
+	return
+}
--- exchange/binance/request_test.go
+++ exchange/binance/request_test.go
@@ -1,1 +1,49 @@
 package binance_test
+
+import "github.com/ramezanius/crypex/exchange/binance"
+
+func (suite *binanceSuite) TestGetSymbols() {
+	symbols, err := suite.Exchange.GetSymbols()
+
+	suite.NoError(err)
+	suite.NotEmpty(symbols)
+}
+
+func (suite *binanceSuite) TestGetBalances() {
+	balances, err := suite.Exchange.GetBalances()
+
+	suite.NoError(err)
+	suite.NotEmpty(balances)
+}
+
+func (suite *binanceSuite) TestOrders() {
+	newRequest := binance.NewOrder{
+		Price:    10,
+		Quantity: 1,
+
+		Side:        binance.Buy,
+		Type:        binance.Limit,
+		Symbol:      binance.BNB + binance.USD,
+		TimeInForce: binance.GoodTillCancel,
+	}
+
+	suite.Run("NewOrder", func() {
+		order, err := suite.Exchange.NewOrder(newRequest)
+
+		suite.NoError(err)
+		suite.NotEmpty(order)
+
+		suite.Equal(order.Side, newRequest.Side)
+		suite.Equal(order.Price, newRequest.Price)
+		suite.Equal(order.Quantity, newRequest.Quantity)
+
+		newRequest.OrderID = order.OrderID
+	})
+	suite.Run("CancelOrder", func() {
+		order, err := suite.Exchange.CancelOrder(
+			newRequest.OrderID, newRequest.Symbol)
+
+		suite.NoError(err)
+		suite.NotEmpty(order)
+	})
+}
