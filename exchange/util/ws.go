b82eab4cbdcbe9c6ce693993b816ddd5f08c03af
Sadegh Ramezani <sadegh@ramezaniu.me>
2020-07-23 21:16:23 +0430

    [fix] Remove condition that never happens

--- exchange/util/ws.go
+++ exchange/util/ws.go
@@ -9,14 +9,10 @@ import (
 
 // GenerateSignature generates a signature from key and writes it to q.
 func GenerateSignature(key string, q url.Values) string {
-	if key != "" {
-		mac := hmac.New(sha256.New, []byte(key))
-		mac.Write([]byte(q.Encode()))
+	mac := hmac.New(sha256.New, []byte(key))
+	mac.Write([]byte(q.Encode()))
 
-		expectedMAC := mac.Sum(nil)
+	expectedMAC := mac.Sum(nil)
 
-		return hex.EncodeToString(expectedMAC)
-	}
-
-	return ""
+	return hex.EncodeToString(expectedMAC)
 }
