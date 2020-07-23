23b443ac8eac3d52c93eafa629b90af7519c56f0
Sadegh Ramezani <sadegh@ramezaniu.me>
2020-07-23 21:15:29 +0430

    [dock] Update 'GenerateUUID' comment header

--- exchange/util/uuid.go
+++ exchange/util/uuid.go
@@ -6,7 +6,7 @@ import (
 	uuid "github.com/satori/go.uuid"
 )
 
-// GenerateUUID used as order id generator in NewOrder, ReplaceOrder
+// GenerateUUID used as order id generator in NewOrder
 func GenerateUUID() string {
 	return strings.ReplaceAll(uuid.NewV4().String(), "-", "")
 }
