719463c8347c76f623a767bcb418475e2e953d96
Sadegh Ramezani <sadegh@ramezaniu.me>
2020-07-23 21:11:03 +0430

    [fix] Fix exchange params
    * Remove feeds

--- exchange/types.go
+++ exchange/types.go
@@ -1,24 +1,18 @@
 package exchange
 
-const (
-	// Exchange feeds
-	ReportsFeed = "REPORTS"
-	CandlesFeed = "CANDLES::"
-)
-
-// RequestParams api request struct
+// RequestParams struct
 type RequestParams struct {
 	Auth     bool
-	Method   string
 	Endpoint string
+	Method   string
 	Params   interface{}
 }
 
-// StreamParams stream request struct
+// StreamParams struct
 type StreamParams struct {
-	Auth     bool   `json:"-"`
-	Endpoint string `json:"-"`
-
-	Method string      `json:"method"`
-	Params interface{} `json:"params"`
+	Auth     bool        `json:"-"`
+	Location string      `json:"-"`
+	Endpoint string      `json:"-"`
+	Method   string      `json:"method"`
+	Params   interface{} `json:"params"`
 }
