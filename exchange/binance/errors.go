cb4aeef1228d170150469c39a8892ea8f30cc8f8
Sadegh Ramezani <sadegh@ramezaniu.me>
2020-07-24 12:13:11 +0430

    [feat] Add errors

--- exchange/binance/errors.go
+++ exchange/binance/errors.go
@@ -1,10 +1,22 @@
 package binance
 
+import "fmt"
+
+// APIError struct
 type APIError struct {
 	Code    int    `json:"code"`
 	Message string `json:"msg"`
 }
 
-func (e APIError) String() string {
+func (e *APIError) Error() string {
 	return e.Message
 }
+
+var (
+	// ErrCurrencyNotFound error
+	ErrCurrencyNotFound = fmt.Errorf("currency not found")
+	// ErrSymbolNotFound error
+	ErrSymbolNotFound = fmt.Errorf("symbol not found")
+	// ErrKeysNotSet error
+	ErrKeysNotSet = fmt.Errorf("binance api keys are not set")
+)
--- exchange/hitbtc/errors.go
+++ exchange/hitbtc/errors.go
@@ -1,13 +1,24 @@
 package hitbtc
 
+import "fmt"
+
 type APIError struct {
-	Error struct {
+	Err struct {
 		Code        int    `json:"code"`
 		Message     string `json:"message"`
 		Description string `json:"description"`
 	} `json:"error"`
 }
 
-func (e APIError) String() string {
-	return e.Error.Message
+func (e *APIError) Error() string {
+	return e.Err.Message
 }
+
+var (
+	// ErrCurrencyNotFound error
+	ErrCurrencyNotFound = fmt.Errorf("currency not found")
+	// ErrSymbolNotFound error
+	ErrSymbolNotFound = fmt.Errorf("symbol not found")
+	// ErrKeysNotSet error
+	ErrKeysNotSet = fmt.Errorf("hitbtc api keys not set")
+)
