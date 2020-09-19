43652a2a39ee39272b3978eb8847eaf1c516592b
Sadegh Ramezani <me@ramezanius.me>
2020-09-19 18:57:56 +0430

    [doc] Update examples

--- example/stream/binance/stream.go
+++ example/stream/binance/stream.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"sync"
 
 	"github.com/gopod/crypex/exchange/binance"
 )
@@ -11,46 +12,41 @@ var Binance *binance.Binance
 func main() {
 	Binance = binance.New()
 
-	Binance.SetStreams(func(response interface{}) {
-		log.Println("Binance[Klines] received:", response)
-	}, func(response interface{}) {
-		log.Println("Binance[Reports] received:", response)
-	})
-
 	Binance.PublicKey = "YOUR_BINANCE_PUBLIC_KEY"
 	Binance.SecretKey = "YOUR_BINANCE_SECRET_KEY"
 
-	SubscribeReports()
-	SubscribeCandles(binance.CandlesParams{
-		Symbol: binance.BNB + binance.USD,
-		Period: binance.Period1Minute,
-	})
-}
+	wg := sync.WaitGroup{}
+	wg.Add(1)
 
-func SubscribeReports() {
-	defer func() {
-		err := Binance.UnsubscribeReports()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
+	go receiveStreams()
 
 	err := Binance.SubscribeReports()
 	if err != nil {
 		log.Panic(err)
 	}
-}
 
-func SubscribeCandles(params binance.CandlesParams) {
-	defer func() {
-		err := Binance.UnsubscribeCandles(params)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	err := Binance.SubscribeCandles(params)
+	err = Binance.SubscribeCandles(
+		binance.CandlesParams{
+			Symbol: binance.BTC + binance.USD,
+			Period: binance.Period1Minute,
+		})
 	if err != nil {
 		log.Panic(err)
 	}
+
+	wg.Wait()
+}
+
+func receiveStreams() {
+	go func() {
+		for candles := range Binance.Feeds.Candles {
+			log.Println(candles)
+		}
+	}()
+
+	go func() {
+		for report := range Binance.Feeds.Reports {
+			log.Println(report)
+		}
+	}()
 }
--- example/stream/hitbtc/stream.go
+++ example/stream/hitbtc/stream.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"sync"
 
 	"github.com/gopod/crypex/exchange/hitbtc"
 )
@@ -11,46 +12,41 @@ var HitBTC *hitbtc.HitBTC
 func main() {
 	HitBTC = hitbtc.New()
 
-	HitBTC.SetStreams(func(response interface{}) {
-		log.Println("HitBTC[Candles] received:", response)
-	}, func(response interface{}) {
-		log.Println("HitBTC[Reports] received:", response)
-	})
-
 	HitBTC.PublicKey = "YOUR_HITBTC_PUBLIC_KEY"
 	HitBTC.SecretKey = "YOUR_HITBTC_SECRET_KEY"
 
-	SubscribeReports()
-	SubscribeCandles(hitbtc.CandlesParams{
-		Symbol: hitbtc.BTC + hitbtc.USD,
-		Period: hitbtc.Period1Minute,
-	})
-}
+	wg := sync.WaitGroup{}
+	wg.Add(1)
 
-func SubscribeReports() {
-	defer func() {
-		err := HitBTC.UnsubscribeReports()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
+	go receiveStreams()
 
 	err := HitBTC.SubscribeReports()
 	if err != nil {
 		log.Panic(err)
 	}
-}
 
-func SubscribeCandles(params hitbtc.CandlesParams) {
-	defer func() {
-		err := HitBTC.UnsubscribeCandles(params)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	err := HitBTC.SubscribeCandles(params)
+	err = HitBTC.SubscribeCandles(
+		hitbtc.CandlesParams{
+			Symbol: hitbtc.BTC + hitbtc.USD,
+			Period: hitbtc.Period1Minute,
+		})
 	if err != nil {
 		log.Panic(err)
 	}
+
+	wg.Wait()
+}
+
+func receiveStreams() {
+	go func() {
+		for candles := range HitBTC.Feeds.Candles {
+			log.Println(candles)
+		}
+	}()
+
+	go func() {
+		for report := range HitBTC.Feeds.Reports {
+			log.Println(report)
+		}
+	}()
 }
