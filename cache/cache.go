84062873cefd6e71fb1c317b22db8f292c6b4772
Sadegh Ramezani <me@ramezanius.me>
2020-09-19 18:52:06 +0430

    [doc] Add comments to cache package

--- cache/cache.go
+++ cache/cache.go
@@ -8,10 +8,12 @@ import (
 	"github.com/allegro/bigcache"
 )
 
+// Cache cache storage for data loader
 type Cache struct {
 	cache *bigcache.BigCache
 }
 
+// NewCache returns a new cache object.
 func NewCache(maxLimit int) (*Cache, error) {
 	cache, err := bigcache.NewBigCache(
 		bigcache.Config{
@@ -27,14 +29,17 @@ func NewCache(maxLimit int) (*Cache, error) {
 	return &Cache{cache}, nil
 }
 
+// Prefix type
 type Prefix int
 
 const (
+	// Data prefixes
 	Candles Prefix = iota
 	Symbol
 	Price
 )
 
+// GetPrefix returns the prefix with arguments.
 func GetPrefix(prefix Prefix, opts ...string) string {
 	switch prefix {
 	case Candles:
@@ -48,12 +53,14 @@ func GetPrefix(prefix Prefix, opts ...string) string {
 	return strings.Join(opts, ":")
 }
 
+// Store stores a entry to the specific key on cache object.
 func (c *Cache) Store(key string, entry []byte) (err error) {
 	err = c.cache.Set(key, entry)
 
 	return
 }
 
+// Load loads entry from cache an bind it to the v.
 func (c *Cache) Load(key string, v interface{}) (err error) {
 	entry, err := c.cache.Get(key)
 	if err != nil {
